7e378a37921702260620e7ec04d48fa2f9f89c70
Jackson Congdon <jackson.congdon@gmail.com>
2019-07-06 09:46:17 -0500

    Create 5.02.3: Basic while loop expression.cpp

---
+++ zyBooks/05. Loops/5.02.3: Basic while loop expression.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+   int userNum = 0;
+
+   userNum = 40;
+
+   /* Your solution goes here  */
+   while (userNum > 1) {
+      userNum = userNum / 2;
+      cout << userNum << " ";
+   }
+   /* Your solution ends here  */
+
+   cout << endl;
+
+   return 0;
+}
