a74b05fa045670f57c4606004332a15475e75f77
Jackson Congdon <jackson.congdon@gmail.com>
2019-07-06 09:35:45 -0500

    Create 5.2.2: Basic while loop with user input.cpp

---
+++ zyBooks/05. Loops/5.2.2: Basic while loop with user input.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+   int userNum = 0;
+
+   userNum = 9;
+   while (/* Your solution goes here  */ userNum >= 0 /* Your solution goes here  */) {
+      cout << "Body" << endl;
+      cin >> userNum;
+   }
+   cout << "Done." << endl;
+
+   return 0;
+}
