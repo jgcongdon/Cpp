294c02783d4df1406b94bcbb218da00751391e94
Jackson Congdon <jackson.congdon@gmail.com>
2019-07-06 19:45:11 -0500

    Create 5.05.3: For loop: Print N to 0.cpp

---
+++ zyBooks/05. Loops/5.05.3: For loop: Print N to 0.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+   int userNum = 0;
+   int i = 0;
+
+   userNum = 3;
+
+   /* Your solution goes here  */
+   for (i=userNum; i>=1; --i) {
+      cout << i << endl;
+   }
+   
+   cout << "Blastoff!" << endl;
+   /* Your solution ends here  */
+
+   return 0;
+}
