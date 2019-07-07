76e70c90003560a92ad49af1073a8b7cbdd5d5c1
Jackson Congdon <jackson.congdon@gmail.com>
2019-07-06 19:39:27 -0500

    Create 5.04.1: While loop: Print 1 to N.cpp

---
+++ zyBooks/05. Loops/5.04.1: While loop: Print 1 to N.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+   int userNum = 0;
+   int i = 0;
+
+   userNum = 4;    // Assume positive
+
+   /* Your solution goes here  */
+   i = 1;
+   while (i <= userNum){
+      cout << i << " ";
+      ++i;
+   }
+   /* Your solution ends here  */
+
+   cout << endl;
+
+   return 0;
+}
