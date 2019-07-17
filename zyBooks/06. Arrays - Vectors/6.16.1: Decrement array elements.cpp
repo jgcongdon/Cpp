8fb7cc1ffad6085ab718308a8f36c93b0715036a
Jackson Congdon <jackson.congdon@gmail.com>
2019-07-16 19:30:45 -0500

    Create 6.16.1: Decrement array elements.cpp

---
+++ zyBooks/06. Arrays - Vectors/6.16.1: Decrement array elements.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+   const int SCORES_SIZE = 4;
+   int lowerScores[SCORES_SIZE];
+   int i = 0;
+
+   lowerScores[0] = 5;
+   lowerScores[1] = 0;
+   lowerScores[2] = 2;
+   lowerScores[3] = -3;
+
+   /* Your solution goes here  */
+   for (i=0; i < SCORES_SIZE; i++) {
+      if (lowerScores[i] > 0) {
+         lowerScores[i] = lowerScores[i] - 1;
+      }
+      else {
+         lowerScores[i] = 0;
+      }
+   }
+   /* Your solution ends here  */
+
+   for (i = 0; i < SCORES_SIZE; ++i) {
+      cout << lowerScores[i] << " ";
+   }
+   cout << endl;
+
+   return 0;
+}
