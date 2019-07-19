78130f5bc64d74807d25cfa22e687dbada7367df
Jackson Congdon <jackson.congdon@gmail.com>
2019-07-19 15:11:07 -0500

    Create 7.01.2: Basic function call.cpp

---
+++ zyBooks/07. User-Defined Functions/7.01.2: Basic function call.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+void PrintShape() {
+
+   /* Your solution goes here  */
+   cout << "***" << endl << "***" << endl << "***" << endl;
+   /* Your solution ends here  */
+
+   return;
+}
+
+int main() {
+   PrintShape();
+
+   return 0;
+}
