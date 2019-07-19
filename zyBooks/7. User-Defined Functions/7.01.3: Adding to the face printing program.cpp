9be0ade1bae1dc8b5bcccb6683110cd3772ec3b4
Jackson Congdon <jackson.congdon@gmail.com>
2019-07-19 15:08:55 -0500

    Create 7.01.3: Adding to the face printing program.cpp

---
+++ zyBooks/7. User-Defined Functions/7.01.3: Adding to the face printing program.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+void PrintPattern() {
+
+   /* Your solution goes here  */
+   cout << "*****";
+   /* Your solution ends here  */
+
+}
+
+int main() {
+   PrintPattern();
+   PrintPattern();
+   cout << endl;
+   return 0;
+}
