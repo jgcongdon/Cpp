ed57e5dc3f49b7aa86e2a9a031523d5c084ea602
Jackson Congdon <jackson.congdon@gmail.com>
2019-07-24 11:30:34 -0500

    Create 7.02.3: Function call with parameter: Printing formatted measurement.cpp

---
+++ zyBooks/07. User-Defined Functions/7.02.3: Function call with parameter: Printing formatted measurement.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+/* Your solution goes here  */
+void PrintFeetInchShort(int numFeet, int numInches) {
+   cout << numFeet << "' " << numInches << "\"";
+   return;
+}
+/* Your solution ends here  */
+
+int main() {
+   PrintFeetInchShort(5, 8);
+   cout << endl;
+
+   return 0;
+}
