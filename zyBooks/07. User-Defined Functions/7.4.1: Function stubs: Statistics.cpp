69c945eb4f48c8cbb7b7a374e7349e359c5c0903
Jackson Congdon <jackson.congdon@gmail.com>
2019-08-09 10:26:29 -0500

    Create 7.4.1: Function stubs: Statistics.cpp

---
+++ zyBooks/07. User-Defined Functions/7.4.1: Function stubs: Statistics.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+/* Your solution goes here  */
+   int GetUserNum(){
+	   cout << "FIXME: Finish GetUserNum()";
+	   cout << endl;
+	   return -1;
+	   }
+
+   int ComputeAvg(int, int){
+	   cout << "FIXME: Finish ComputeAvg()";
+	   cout << endl;
+	   return -1;
+      }
+/* Your solution ends here  */
+
+int main() {
+   int userNum1 = 0;
+   int userNum2 = 0;
+   int avgResult = 0;
+
+   userNum1 = GetUserNum();
+   userNum2 = GetUserNum();
+
+   avgResult = ComputeAvg(userNum1, userNum2);
+
+   cout << "Avg: " << avgResult << endl;
+
+   return 0;
+}
