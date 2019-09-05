42755e842bfdc4dd4a678238b25beb524527dec3
Jackson Congdon <jackson.congdon@gmail.com>
2019-09-05 15:24:44 -0500

    Create 10.03.3: Output formatting: Printing a maximum number of digits in the fraction.cpp

---
+++ zyBooks/10. Streams/10.03.3: Output formatting: Printing a maximum number of digits in the fraction.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <ios>
+#include <iomanip>
+using namespace std;
+
+int main() {
+   double outsideTemperature = 103.45632;
+
+   /* Your solution goes here  */
+   cout << setprecision(2) << fixed << outsideTemperature << endl;
+   /* Your solution ends here  */
+
+   return 0;
+}
