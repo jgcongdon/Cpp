73d103e6edc20670bbc6a33b5a7d14c9d6fc7530
Jackson Congdon <jackson.congdon@gmail.com>
2019-09-11 16:22:40 -0500

    Update Student.h

--- Scripting and Programming Applications - C867/Performance Assessment - Task 1 - Class Roster/Student.h
+++ Scripting and Programming Applications - C867/Performance Assessment - Task 1 - Class Roster/Student.h
@@ -1,52 +1,109 @@
-#pragma once
-#include <string>
-#include "degree.h"
-using std::string;
-
- 
-class Student {
-
-
-public:
-const static int daysCourseArraySize = 3;
+#include<iostream>
+#include<iomanip>
+#include "student.h"
+using std::cout;
+using std::left;
+using::setw;
 
 
-protected:
-string studentID;
-string firstName;
-string lastName;
-string emailAddress;
-int age;
-int daysCourse[daysCourseArraySize];
-Degree dtype;
+student::student()
+{
+    this->studentID = "";
+    this->firstName = "";
+    this->lastName = "";
+    this->emailAddress = "";
+    this->age = 0;
+    for (int i = 0; i < daysCourseArraySize; i++) this->daysCourse[i] = 0;
+}
 
 
-public:
-Student();
-student (string ID, string firstName, string lastName, string emailAddress,
-	   int age, int daysCourse[]);
+student::student(string ID, string firstName, string lastName, string emailAddress, int age, int daysCourse[])
+{
+    this->studentID = ID;
+    this->firstName = firstName;
+    this->lastName = lastName;
+    this->emailAddress = emailAddress;
+    this->age = age;
+    for (int i = 0; i < daysCourseArraySize; i++) this->daysCourse[i] = daysCourse[i];
+}
 
 
-string getID();
-string getFirstName();
-string getLastName();
-string getEmailAddress();
-int getAge();
-int* getDaysCourse();
-virtual Degree getDegree() = 0;
+string student::getID()
+{
+    return studentID;
+}
+
+string student::getFirstName()
+{
+    return firstName;
+}
+
+string student::getLastName()
+{
+    return lastName;
+}
+
+string student::getEmailAddress()
+{
+    return emailAddress;
+}
+
+int student::getAge()
+{
+    return age;
+}
+
+int * student::getDaysCourse()
+{
+    return daysCourse;
+}
 
 
-void setID(string ID);
-void setFirstName(string firstName);
-void setLastName(string lastName);
-void setEmailAddress(string emailAddress);
-void setAge(int age);
-void setDaysCourse(int daysCourse[]);
+void student::setID(string ID)
+{
+    this->studentID = ID;
+}
 
-virtual void print() = 0;
-  
-  
-~student();
+void student::setFirstName(string firstName)
+{
+    this->firstName = firstName;
+}
+
+void student::setLastName(string lastName)
+{
+    this->lastName = lastName;
+}
+
+void student::setEmailAddress(string emailAddress)
+{
+    this->emailAddress = emailAddress;
+}
+
+void student::setAge(int age)
+{
+    this->age = age;
+}
+
+void student::setDaysCourse(int daysCourse[])
+{
+    for (int i = 0; i < daysCourseArraySize; i++)
+    this->daysCourse[i] = daysCourse[i];
+}
 
 
-};
+void student::print()
+{
+    cout << studentID;
+    cout << firstName;
+    cout << lastName;
+    cout << emailAddress;
+    cout << age;
+    cout << daysCourse[0];
+    cout << daysCourse[1];
+    cout << daysCourse[2];
+}
+
+student::~student()
+{
+    
+}
