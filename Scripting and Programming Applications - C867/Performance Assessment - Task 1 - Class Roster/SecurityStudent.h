251926dd55bc7e12765b31086fd96b62fe60872f
Jackson Congdon <jackson.congdon@gmail.com>
2019-09-11 14:58:08 -0500

    Create SecurityStudent.h

---
+++ Scripting and Programming Applications - C867/Performance Assessment - Task 1 - Class Roster/SecurityStudent.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <string>
+#include "student.h"
+class SecurityStudent : public Student
+{
+    public:
+    SecurityStudent();
+    SecurityStudent(
+        string studentID,
+        string firstName,
+        string lastName,
+        string emailAddress,
+        int age,
+        int daysCourse[],
+        Degree degreetype
+        );
+        
+    Degree getDegree();
+    void print();
+    
+    ~SecurityStudent();
+};
