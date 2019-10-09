162425ef75d0abb10ec02c2544ca1825f118c325
Jackson Congdon <jackson.congdon@gmail.com>
2019-10-08 19:48:03 -0500

    Update networkStudent.cpp

--- Scripting and Programming Applications - C867/Performance Assessment - Task 1 - Class Roster/networkStudent.cpp
+++ Scripting and Programming Applications - C867/Performance Assessment - Task 1 - Class Roster/networkStudent.cpp
@@ -7,26 +7,3 @@ Degree networkStudent::getDegree()
 {
 	return degree;
 }
-
-//networkStudent::networkStudent() :Student()
-//{
-//	
-//}
-//
-//networkStudent::networkStudent(
-//	string ID,
-//	string firstName,
-//	string lastName,
-//	string emailAddress,
-//	int age,
-//	int* daysCourse,
-//	Degree degreeType)
-//	:Student(ID, firstName, lastName, emailAddress, age, daysCourse)
-//{
-//	this->degree = degreeType;
-//}
-//
-//void networkStudent::print()
-//{
-//    this->Student::print();
-//}
