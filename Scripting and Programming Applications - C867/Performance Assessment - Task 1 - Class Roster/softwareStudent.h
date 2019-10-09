9d0045826d6847dd1e9ac6eaf33cc70cdd1fa018
Jackson Congdon <jackson.congdon@gmail.com>
2019-10-08 19:51:36 -0500

    Update softwareStudent.h

--- Scripting and Programming Applications - C867/Performance Assessment - Task 1 - Class Roster/softwareStudent.h
+++ Scripting and Programming Applications - C867/Performance Assessment - Task 1 - Class Roster/softwareStudent.h
@@ -4,8 +4,6 @@
 #include "degree.h"
 using namespace std;
 
-
-
 class softwareStudent : public Student
 {
 private:
@@ -14,19 +12,4 @@ private:
 public:
 	using Student::Student;
 	Degree getDegree() override;
-	
-	/*softwareStudent();
-	softwareStudent(
-		string ID,
-		string firstName,
-		string lastName,
-		string emailAddress,
-		int age,
-		int* daysCourse,
-		Degree degreeType
-	);
-
-	Degree getDegree();
-	void print();*/
-
 };
