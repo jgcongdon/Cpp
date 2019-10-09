a0fc43956dc668980303a6c96077d25ea7c8e5fc
Jackson Congdon <jackson.congdon@gmail.com>
2019-10-08 19:49:49 -0500

    Update securityStudent.cpp

--- Scripting and Programming Applications - C867/Performance Assessment - Task 1 - Class Roster/securityStudent.cpp
+++ Scripting and Programming Applications - C867/Performance Assessment - Task 1 - Class Roster/securityStudent.cpp
@@ -7,26 +7,3 @@ Degree securityStudent::getDegree()
 {
 	return degree;
 }
-
-//securityStudent::securityStudent() :Student()
-//{
-//
-//}
-//
-//securityStudent::securityStudent(
-//	string ID,
-//	string firstName,
-//	string lastName,
-//	string emailAddress,
-//	int age,
-//	int* daysCourse,
-//	Degree degreeType)
-//	: Student(ID, firstName, lastName, emailAddress, age, daysCourse)
-//{
-//	this->degree = degreeType;
-//}
-//
-//void securityStudent::print()
-//{
-//	this->Student::print();
-//}
